419080dab5e4844e656afaa546bc04d0ed47ef60
Boaz Rosenan <brosenan@gmail.com>
2018-12-17 22:22:12 +0200

    docs

---
+++ docs/implementors/std/error/trait.Error.js
@@ -0,0 +1,12 @@
+(function() {var implementors = {};
+implementors["pest"] = [{text:"impl&lt;'i, R:&nbsp;<a class=\"trait\" href=\"pest/trait.RuleType.html\" title=\"trait pest::RuleType\">RuleType</a>&gt; <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"pest/error/struct.Error.html\" title=\"struct pest::error::Error\">Error</a>&lt;R&gt;",synthetic:false,types:["pest::error::Error"]},];
+implementors["syn"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"struct\" href=\"syn/synom/struct.ParseError.html\" title=\"struct syn::synom::ParseError\">ParseError</a>",synthetic:false,types:["syn::error::ParseError"]},];
+implementors["ucd_trie"] = [{text:"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/std/error/trait.Error.html\" title=\"trait std::error::Error\">Error</a> for <a class=\"enum\" href=\"ucd_trie/enum.Error.html\" title=\"enum ucd_trie::Error\">Error</a>",synthetic:false,types:["ucd_trie::owned::Error"]},];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
